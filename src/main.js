b4b42abb718d4c3d281da485b411ef34dacc2b01
DanielMart1ns <danielmmrodrigues09@gmail.com>
2024-01-16 14:14:52 -0300

    feat: inclusão da seção FAQ e footer

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     
     const buttons = document.querySelectorAll('[data-tab-button]')
+    const questions = document.querySelectorAll('[data-faq-question]')
 
     for(let i=0; i<buttons.length; i++){
         buttons[i].addEventListener('click', (botao)=>{
@@ -14,6 +15,10 @@ document.addEventListener('DOMContentLoaded', ()=>{
             botaoSelecionado.classList.add('shows__tabs__button--is-active')
             aba.classList.add('shows__list--is-active');
         })
+    }
+
+    for (let i=0; i<questions.length; i++){
+        questions[i].addEventListener('click', abreOuFechaResposta)
     }
 })
 
@@ -31,4 +36,11 @@ function escondeTodasAbas (){
     for(let i=0; i<tabsContainer.length; i++){
         tabsContainer[i].classList.remove('shows__list--is-active')
     }
+}
+
+function abreOuFechaResposta(elemento){
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe)
 }
